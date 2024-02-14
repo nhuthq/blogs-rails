b9e7d36848175a52214af87c29201c8f3e8234aa
nhuthq <>
2024-02-14 14:44:02 +0700

    add Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_014353) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_26_075411) do
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
